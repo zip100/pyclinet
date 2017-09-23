f4d7757f3737b8c3026082d18a8bd050bde995d3
mike <memcache@qq.com>
2017-09-23 21:32:35 +0800

    init

--- Camera.py
+++
@@ -1,5 +0,0 @@
-class Camera:
-    posX, posY = 1500;
-
-    def move(self, x, y):
-        return
--- main.py
+++ main.py
@@ -2,14 +2,50 @@
 
 import thread
 import socket
-import json
-import time
 import struct
+import serial
 
 x = 0
 y = 0
 
 package = "!BBiiB"
+
+
+class Camera:
+    p1 = 1500
+    p2 = 1500
+    ser = 0;
+
+    def __init__(self):
+        self.ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        command = "#9P%(p1)d#15P%(p2)dT100\r\n" % {'p1': self.p1, 'p2': self.p2}
+        self.send(command)
+
+    def send(self, command):
+        self.ser.write(command.encode())
+
+    def move(self, x, y):
+        pp1 = self.p1 - x
+        if (pp1 > 2400):
+            pp1 = 2400
+        if (pp1 < 600):
+            pp1 = 600
+        self.p1 = pp1
+
+        pp2 = self.p2 - y
+        if (pp2 > 2400):
+            pp2 = 2400
+        if (pp2 < 600):
+            pp2 = 600
+        self.p2 = pp2
+
+        command = "#9P%(p1)d#15P%(p2)dT100\r\n" % {'p1': self.p1, 'p2': self.p2}
+        self.send(command)
+
+        return
+
+
+camera = Camera()
 
 
 def worker(connection):
@@ -32,14 +68,14 @@ def worker(connection):
                 y = data[3]
             if (data[4] == 7):
                 moveX = int((data[2] - x) * 0.54)
-                moveY = int((data[3] - y) / 0.74)
+                moveY = int((data[3] - y) / 0.54)
 
                 if (moveY > 100 or moveY < -100 or moveX > 100 or moveX < -100):
-                    print moveX, moveY
+                    camera.move(moveX, moveY)
                     x = data[2]
                     y = data[3]
+                    print "------------------------"
 
-            print "------------------------"
 
         except socket.timeout:
             print "[System] Socket Timeout..."
